dd9ded2771d20e1f4c24b7a10fe9e55e715fd906
Rohan-ks99 <kashyaprohan314@gmail.com>
2026-01-29 16:45:15 +0000

    container

---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,18 @@
+" use strict";// treat all js code as newer version
+//aleart(3+3) // we are using nodejs ,not browser
+console.log(3+3);
+// code readability should be high
+console.log("Rohan")
+let name = "Rohan"
+let age = 18
+let isLoggedin =false;
+// number =>2 to power 53
+// bigint
+//string=>""
+//boolean=>true or false
+//null=> standalone value
+// undefined =>
+// symbol =>unique  
+// object   
+console.log(typeof undefined);// return undefined
+console.log(typeof null);// return object
