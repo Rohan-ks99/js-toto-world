99766781b7bee8caa19a753f22e27b5ef6298a7f
Rohan-ks99 <kashyaprohan314@gmail.com>
2026-07-08 17:24:59 +0000

    comparision in basics of js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log("2">1);
+// console.log("02" >  1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+// // the reason is that an equality check == and comparisions <,>,>=,<= work differently.
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined<0);
+//=== => strict check
+console.log("2" === 2);
