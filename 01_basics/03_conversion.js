982528c996a6b48d8fc216006acea1396b788447
Rohan-ks99 <kashyaprohan314@gmail.com>
2026-07-08 16:41:06 +0000

    add new conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -5,7 +5,7 @@ let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 //"33" => 33
-//"33abc" => NaN
+//"33abc" => NaN => not a number 
 //true => 1;false =>0
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
@@ -17,4 +17,29 @@ let s =33
 let stringNumber = String(s)
 console.log(stringNumber);
  console.log(typeof stringNumber);
- 
+ // ****************************** Operations*********************//
+ let value  =3 
+ let negvalue =-value
+//  console.log(negvalue);
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**2);
+//  console.log(2/2);
+//  console.log(2%2);
+let str  ="hello"
+let str2 = " Rohan"
+let str3 = str + str2
+console.log(str3);
+console.log("1" + 2)
+console.log("1" + 2 +3)
+console.log(1 + "2" )
+console.log(1 + 2 +"3");
+
+//console.log(+true) => 1
+//console.log(true) => true
+let game =100
+game++;
+console.log(game)
+//study this conversion 
+
