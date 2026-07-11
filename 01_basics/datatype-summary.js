61e1e2968baa2d79366bc0d84d6aecfe0e742ffd
Rohan-ks99 <kashyaprohan314@gmail.com>
2026-07-11 04:53:23 +0000

    datatypesummary

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -3,12 +3,24 @@
 const score = 100 
 const scValue =100.3
 const isLoggedIn =false 
-const outsideTWemp = null 
+const outsideTWemp = null // the returntype of null is object 
 let userEmail;
 const id = Symbol('123')
 const anotherId =Symbol('123')
 console.log(id === anotherId);
+const bigNumber = 34296745n // the returntype of bigint is undefined 
 
 // Reference (Non primitive)
 
 // Array,Objects,Functions
+
+ const heros =["spiderman","tyler","batman"] // the return type of the array is object 
+ let myObj ={
+    name :"rohan",
+    age : 22 ,
+ } /// the return type of object is object 
+ const myFunction  = function(){
+   console.log("Hello world");
+ }// the return type of the fuction is funtion object 
+ console.log(typeof myFunction);
+ console.log(myFunction);
