abf37c08bfb20863ad7920bffd38bc881bdfcb3e
Rohan-ks99 <kashyaprohan314@gmail.com>
2026-09-13 03:40:48 +0000

    arr2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,27 @@
+const marval_heros =["thor","ironman","spiderman"]
+const dc_heros =["batman","superman","flash"]
+//marval_heros.push(dc_heros)
+console.log(marval_heros);
+ const allhero = marval_heros.concat(dc_heros)
+console.log(allhero);
+const all_new_heros = [...marval_heros,...dc_heros]
+console.log(all_new_heros);
+
+const another_array = [1,2,3[4,5,6],7,[6,7,[4,5]]]
+const  real_another_array =  another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("rohan"));
+console.log(Array.from("rohan"));
+console.log(Array.from({name:"rohan"})); // interesting
+
+let score1 =100
+let score2 =200
+let score3 =300
+console.log(Array.of(score1,score2,score3));
+
+
+
+
